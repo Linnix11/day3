f1732d56ee39a9d42c17e6e9cb2c3bae54813a6c
Linnix11 <arthur.champagne-forterre@epitech.digital>
2024-10-02 11:24:46 +0200

    ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,9 @@
+function getAngryDog(numberOfWoofs) {
+    let result = "woof";  
+    
+    for (let i = 0; i < numberOfWoofs; i++) {
+        result += "woof";  
+    }
+    
+    return result;  
+}
